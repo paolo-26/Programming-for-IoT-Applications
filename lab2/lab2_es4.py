52ea7e7172f2febe776e6fa4ce83011bc5f530a5
paolo-26 <paolo.1995@ymail.com>
2018-11-14 12:27:05 +0100

    Add POST method

--- lab2/lab2_es4.py
+++ lab2/lab2_es4.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
 """
 @author = Paolo Grasso
+
+Visit 0.0.0.0/8080
 """
 import json
 import cherrypy
 
 class Index(object):
     exposed = True
+
     def GET(self):
         return open("./freeboard/index.html", "r").read()
 
+class SaveDashboard(object):
+    exposed = True
+
+    def POST(self, *uri, **params):
+        dash_json = json.loads(params["json_string"])  # Load json object
+        with open("./freeboard/dashboard/dashboard.json", "w") as f:
+            json.dump(dash_json, f)  # Write json to file
+
 if __name__ == '__main__':
     cherrypy.tree.mount (Index(), '/', config='conf')
+    cherrypy.tree.mount (SaveDashboard(), '/saveDashboard', config='conf')
     cherrypy.config.update('conf')
     cherrypy.engine.start()
     cherrypy.engine.block()

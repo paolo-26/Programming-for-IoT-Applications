a397eac1dd32bd77bf105cb8dc834cf14dbc59bc
paolo-26 <paolo.1995@ymail.com>
2018-12-03 15:34:44 +0100

    Add file

---
+++ lab4/lab4_es2_pub.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+@author = Paolo Grasso
+"""
+
+import paho.mqtt.client as PahoMQTT
+import time
+import datetime
+import json
+
+BROKER_IP = '192.168.1.28'
+
+class MyPublisher(object):
+    def __init__(self, clientID, serverIP):
+        self.clientID = clientID
+        self.messageBroker = serverIP
+        self._paho_mqtt = PahoMQTT.Client(self.clientID, False)
+        self._paho_mqtt.on_connect = self.my_on_connect  # Association
+
+    def start(self):
+        self._paho_mqtt.connect(self.messageBroker, 1883)
+        self._paho_mqtt.loop_start()
+
+    def stop(self):
+        self._paho_mqtt.loop_stop()
+        self._paho_mqtt.disconnect()
+
+    def my_publish(self, topic, message):
+        self._paho_mqtt.publish(topic, message, 2)
+
+    def my_on_connect(self, client, userdata, flags, rc):
+        global loop_flag
+        print ("Connected to %s - Result code: %d" % (self.messageBroker, rc))
+        loop_flag = 0
+
+
+
+
+if __name__ == "__main__":
+    pub = MyPublisher("Pub1", BROKER_IP)
+    loop_flag = 1
+    flag = 1
+    pub.start()
+
+    while loop_flag:
+        print("Waiting for connection...")
+        time.sleep(.01)
+
+    while True:
+        timestamp = str(time.time())
+        jtime = {"timestamp": timestamp}
+        print("Publishing on topic '/timestamp: %s" % (jtime))
+        pub.my_publish('/timestamp', json.dumps(jtime))
+
+        if flag == 1:
+            datemessage = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+            jdate = {"datetime": datemessage}
+            print("Publishing on topic '/datetime: %s" % (jdate))
+            pub.my_publish('/datetime', json.dumps(jdate))
+            flag = 0
+
+        elif flag == 0:
+            flag = 1
+
+        time.sleep(30)
+
+    pub.stop()
---
+++ lab4/lab4_es2_sub2.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+@author = Paolo Grasso
+"""
+
+import paho.mqtt.client as PahoMQTT
+import time
+import datetime
+import json
+
+TOPIC = '/timestamp'
+BROKER_IP = '192.168.1.28'
+
+class MySubscriber:
+    def __init__(self, clientID, topic, serverIP):
+        self.clientID = clientID
+        self.topic = topic
+        self.messageBroker = serverIP
+        self._paho_mqtt = PahoMQTT.Client(clientID, False)
+        self._paho_mqtt.on_connect = self.my_on_connect
+        self._paho_mqtt.on_message = self.my_on_message_received
+
+
+    def start (self):
+        self._paho_mqtt.connect(self.messageBroker, 1883)
+        self._paho_mqtt.loop_start()
+        self._paho_mqtt.subscribe(self.topic, 2)
+
+    def stop (self):
+        self._paho_mqtt.unsubscribe(self.topic)
+        self._paho_mqtt.loop_stop()
+        self._paho_mqtt.disconnect()
+
+    def my_on_connect (self, paho_mqtt, userdata, flags, rc):
+        global loop_flag
+        print ("Connected to %s - Result code: %d" % (self.messageBroker, rc))
+        loop_flag = 0
+
+    def my_on_message_received(self, client, userdata, msg):
+        msg.payload = msg.payload.decode("utf-8")
+        message = json.loads(msg.payload)
+        print("Timestamp: " + message['timestamp']+ "\t[QoS: "+str(msg.qos)+"]")
+
+
+if __name__ == "__main__":
+    sub = MySubscriber("Sub2", TOPIC, BROKER_IP)
+    loop_flag = 1
+    sub.start()
+
+    while loop_flag:
+        print("Waiting for connection...")
+        time.sleep(.01)
+
+    while True:
+        time.sleep(1)
+
+    sub.stop()

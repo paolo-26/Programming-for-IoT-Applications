a4f5a1bc4a82c609b1704a68fcf82b4d0a44ce57
paolo-26 <paolo.1995@ymail.com>
2018-12-14 18:08:25 +0100

    Update delete function

--- lab5/lab5_es1.py
+++ lab5/lab5_es1.py
@@ -17,14 +17,17 @@ FILENAME = "info.json"
 
 
 class MyCatalog(object):
+
     def __init__(self, filename):
         self.filename = filename
 
     def load_file(self):
+
         with open(self.filename, "r") as f:
             self.data = json.loads(f.read())
 
     def save_file(self):
+
         with open(self.filename, "w") as f:
             json.dump(self.data, f, ensure_ascii=False)
 
@@ -38,12 +41,16 @@ class MyCatalog(object):
 
         if id == 'all':
             res = self.data["devices"]
+
         else:
             res = [d for d in self.data["devices"] if d["id"] == id]
+
             try:
                 res = res[0]
+
             except:
                 pass
+
         return res
 
     def get_user(self, id):
@@ -51,24 +58,30 @@ class MyCatalog(object):
 
         if id == 'all':
             res = self.data["users"]
+
         else:
             res = [u for u in self.data["users"] if u["id"] == id]
+
             try:
                 res = res[0]
+
             except:
                 pass
+
         return res
 
     def insert(self, ud, dict):
         self.load_file()
+
         if ud == 'user':
             b = self.check_id('user', dict['id'])
+
             if b:
                 self.data["users"].append(dict)
 
-
         if ud == 'device':
             b = self.check_id('device', dict['id'])
+
             if b:
                 self.data["devices"].append(dict)
 
@@ -77,16 +90,27 @@ class MyCatalog(object):
 
     def check_id(self, ud, id):
         b = 1
+
         if ud == 'user':
             ids = [u['id'] for u in self.data['users']]
+
             if id in ids:
                 b = 0
 
         if ud == 'device':
             ids = [d['id'] for d in self.data['devices']]
+
             if id in ids:
                 b = 0
+
         return b
+
+    def delete(self):
+        self.load_file()
+        old = [d['id'] for d in self.data['devices'] if time.time()-float(d['timestamp']) > 120]
+        if len(old) > 0:
+            self.data['devices'] = [d in self.data['devices'] for d in self.data['devices'] if not d['id'] in old]
+            self.save_file()
 
 class WebServer(object):
     exposed = True
@@ -96,9 +120,6 @@ class WebServer(object):
 
     @cherrypy.tools.json_out()
     def GET(self, *uri, **params):
-
-        # with open(self.filename, "r") as f:
-        #     data = json.loads(f.read())
 
         if len(uri) > 1:
             raise cherrypy.HTTPError(404, "Resource not found")
@@ -111,16 +132,19 @@ class WebServer(object):
         if json_in['par'] == 'devices':
             id = json_in['id']
             info = self.calc.get_device(id)
+
             if len(info) == 0:
                 raise cherrypy.HTTPError(404, "Device not found")
 
         if json_in['par'] == 'users':
             id = json_in['id']
             info = self.calc.get_user(id)
+
             if len(info) == 0:
                 raise cherrypy.HTTPError(404, "User not found")
         try:
             return info
+
         except:
             raise cherrypy.HTTPError(422, "Incorrect search parameter")
 
@@ -130,28 +154,36 @@ class WebServer(object):
     def PUT(self, *uri, **params):
 
         try:
+
             if uri[0] == 'add':
+
                 try:
                     dict = json.loads(cherrypy.request.body.read())
                     par = dict['par']
+
                 except:
                     raise SyntaxError
+
                 if par == 'user':
+
                     try:
                         del dict['par']
                         id = dict['id']
                         name = dict['name']
                         surname = dict['surname']
                         email = dict['email']
+
                     except:
                         raise SyntaxError
 
                     b = self.calc.insert('user',dict)
+
                     if b == 0:
                         print ("exist")
                         raise FileExistsError
 
                 elif par == 'device':
+
                     try:
                         del dict['par']
                         id = dict['id']
@@ -159,77 +191,18 @@ class WebServer(object):
                         res = dict['res']
                         timestamp = {"timestamp": str(time.time())}
                         dict.update(timestamp)
+
                     except:
                         raise SyntaxError
                     b = self.calc.insert('device',dict)
+
                     if b == 0:
                         raise FileExistsError
+
                 else:
                     raise SyntaxError
 
-
-
-
-
-
-                # if uri[1] == 'user':
-                #     with open(self.filename, "r") as f:
-                #         data = json.loads(f.read())
-                #
-                #     try:
-                #         dict = json.loads(cherrypy.request.body.read())
-                #     except:
-                #         raise SyntaxError
-                #
-                #     try:
-                #         id = dict['id']
-                #         name = dict['name']
-                #         surname = dict['surname']
-                #         email = dict['email']
-                #
-                #     except:
-                #         raise SyntaxError
-                #
-                #     check = [d['id'] for d in data['users']]
-                #     if id in check:
-                #         raise FileExistsError
-                #
-                #     data["users"].append(dict)
-                #
-                #     with open(self.filename, "w") as f:
-                #         json.dump(data, f, ensure_ascii=False)
-                #
-                #
-                # elif uri[1] == 'device':
-                #     with open(self.filename, "r") as f:
-                #         data = json.loads(f.read())
-                #     try:
-                #         dict = json.loads(cherrypy.request.body.read())
-                #     except:
-                #         raise SyntaxError
-                #
-                #     try:
-                #         id = dict['id']
-                #         endpoints = dict['end-points']
-                #         res = dict['res']
-                #         timestamp = {"timestamp": str(time.time())}
-                #
-                #     except:
-                #         raise SyntaxError
-                #
-                #     check = [d['id'] for d in data['devices']]
-                #
-                #     if id in check:
-                #         raise FileExistsError
-                #
-                #     dict.update(timestamp)
-                #     data["devices"].append(dict)
-                #
-                #     with open(self.filename, "w") as f:
-                #         json.dump(data, f, ensure_ascii=False)
-
-                # else:
-                #     raise SyntaxError
+            #raise SyntaxError
 
             else:
                 raise SyntaxError
@@ -245,12 +218,11 @@ class WebServer(object):
         cherrypy.response.status = "200"
         return "OK"
 
-    def DELETE(self):
+    def DELETE(self, *uri, **params):
+
         if uri[0] == 'del':
-            with open(self.filename, "r") as f:
-                data = json.loads(f.read())
-            old = [d for d in data['devices'] if time.time()-float(d['timestamp']) > 120]
-            print(old)
+            self.calc.delete()
+            print("deleted")
 
 class OtherThread(Thread):
     def __init__(self, ThreadID):
@@ -261,7 +233,7 @@ class OtherThread(Thread):
         while True:
             print("Ciao")
             r = requests.delete('http://0.0.0.0:8080/del')
-            time.sleep(10)
+            time.sleep(60)
 
 class MainThread(Thread):
     def __init__(self, ThreadID):
@@ -286,4 +258,4 @@ if __name__ == '__main__':
     other_thread = OtherThread(2)
     main_thread.start()
     time.sleep(5)
-    #other_thread.start()
+    other_thread.start()
